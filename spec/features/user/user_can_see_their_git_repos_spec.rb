88bf408aa44f992b7b73b009028cfc300e8250bb
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-11 13:29:35 -0600

    Adds following, followers, and repos fixtures in before each mock setup
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- spec/features/user/user_can_see_their_git_repos_spec.rb
+++ spec/features/user/user_can_see_their_git_repos_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 describe 'as a logged in user on my dashboard' do
   before :each do
-    json_response = File.open("./fixtures/user_repos.json")
+    json_followed_response = File.open("./fixtures/user_followed.json")
+    stub_request(:get, "https://api.github.com/user/following").
+      to_return(status: 200, body: json_followed_response)
+
+    json_followers_response = File.open("./fixtures/user_followers.json")
+    stub_request(:get, "https://api.github.com/user/followers").
+      to_return(status: 200, body: json_followers_response)
+
+    json_repos_response = File.open("./fixtures/user_repos.json")
     stub_request(:get, "https://api.github.com/user/repos").
-      to_return(status: 200, body: json_response)
+      to_return(status: 200, body: json_repos_response)
   end
 
   it 'sees a section for github and their collection of repos' do
-    user = create(:user, git_key: ENV["GITHUB_API_KEY"])
+    user = create(:user, git_key: "bananas")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
