3e3d015f6029ce8b1915bf03b5ae3fe736641bcf
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-11 11:52:14 -0600

    Removes commented out pseudo code from sessions controller
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #does not have access to email to find user by
-    #need to find another way to find or create by
-
-    #does have access to auth_hash
-  
     user = User.find_by(email: params[:session][:email])
-    # if user.git_key.nil?
-    #   user.update(git_key: params["authenticity_token"])
-    #   redirect_to dashboard_path
-    # else
-    #
-    # end
 
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
