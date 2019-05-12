77b57ebe9239eac896a07e86a188fa1e15a7a66c
Noah Flint <noah.flint24@gmail.com>
2019-05-11 18:38:59 -0600

    refactored, removed unused variables

--- spec/features/visitors/visitor_sees_homepage_spec.rb
+++ spec/features/visitors/visitor_sees_homepage_spec.rb
@@ -5,12 +5,7 @@ describe 'Visitor' do
     it 'can see a list of tutorials' do
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
-
-      video1 = create(:video, tutorial_id: tutorial1.id)
-      video2 = create(:video, tutorial_id: tutorial1.id)
-      video3 = create(:video, tutorial_id: tutorial2.id)
-      video4 = create(:video, tutorial_id: tutorial2.id)
-
+      
       visit root_path
 
       expect(page).to have_css('.tutorial', count: 2)
