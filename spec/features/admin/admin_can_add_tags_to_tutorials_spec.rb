0fd63722e0250e3f986b3069f3f2040e918bdb14
Noah Flint <noah.flint24@gmail.com>
2019-05-12 14:52:54 -0600

    removed unused variable and on expectation

--- spec/features/admin/admin_can_add_tags_to_tutorials_spec.rb
+++ spec/features/admin/admin_can_add_tags_to_tutorials_spec.rb
@@ -4,9 +4,10 @@ describe 'An admin user can add tags to tutorials' do
   it 'clicks on the add tag on a tutoral' do
     admin = create(:user, role: 1)
     tutorial = create(:tutorial)
-    video1 = create(:video, tutorial_id: tutorial.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(admin)
 
     visit '/admin/dashboard'
 
@@ -19,10 +20,6 @@ describe 'An admin user can add tags to tutorials' do
     fill_in 'tutorial[tag_list]', with: 'Ruby'
     click_on 'Update Tags'
     visit root_path
-
-    within('.categories') do
-      expect(page).to have_content('Ruby')
-    end
 
     within('.categories') do
       click_on 'Ruby'
