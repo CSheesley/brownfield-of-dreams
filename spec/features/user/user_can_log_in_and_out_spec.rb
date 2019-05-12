2ab7f777aae412d62801fc6c309e14ecdfa6ea21
Noah Flint <noah.flint24@gmail.com>
2019-05-11 19:50:33 -0600

    added before :each block, removed unnecessary expects

--- spec/features/user/user_can_log_in_and_out_spec.rb
+++ spec/features/user/user_can_log_in_and_out_spec.rb
@@ -1,56 +1,36 @@
 require 'rails_helper'
 
 describe 'User' do
+  before :each do
+    @user = create(:user)
+    visit login_path
+  end
+
   it 'user can sign in' do
-    user = create(:user)
-
-    visit '/'
-
-    click_on 'Sign In'
-
-    expect(current_path).to eq(login_path)
-
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: user.password
+    fill_in 'session[email]', with: @user.email
+    fill_in 'session[password]', with: @user.password
 
     click_on 'Log In'
 
     expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content(user.email)
-    expect(page).to have_content(user.first_name)
-    expect(page).to have_content(user.last_name)
+    expect(page).to have_content(@user.email)
   end
 
   it 'can log out' do
-    user = create(:user)
-
-    visit login_path
-
-    fill_in'session[email]', with: user.email
-    fill_in'session[password]', with: user.password
+    fill_in'session[email]', with: @user.email
+    fill_in'session[password]', with: @user.password
 
     click_on 'Log In'
-
-    click_on 'Profile'
-
-    expect(current_path).to eq(dashboard_path)
 
     click_on 'Log Out'
 
     expect(current_path).to eq(root_path)
-    expect(page).to_not have_content(user.first_name)
     expect(page).to have_content('Sign In')
   end
 
   it 'is shown an error when incorrect info is entered' do
-    user = create(:user)
-    fake_email = 'email@email.com'
-    fake_password = '123'
-
-    visit login_path
-
-    fill_in'session[email]', with: fake_email
-    fill_in'session[password]', with: fake_password
+    fill_in'session[email]', with: 'fake@email.com'
+    fill_in'session[password]', with: 'fake_password'
 
     click_on 'Log In'
 
