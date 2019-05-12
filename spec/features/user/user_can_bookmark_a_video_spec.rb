26f29fc445be532955aa709ed8b1474574dc5105
Noah Flint <noah.flint24@gmail.com>
2019-05-11 19:29:50 -0600

    refactored test to use a before each action

--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
 describe 'A registered user' do
+  before :each do
+    @tutorial = create(:tutorial, title: 'How to Tie Your Shoes')
+    @video = create(:video, tutorial: @tutorial)
+    @user = create(:user)
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(@user)
+
+    visit tutorial_path(@tutorial)
+  end
+
   it 'can add videos to their bookmarks' do
-    tutorial = create(:tutorial, title: 'How to Tie Your Shoes')
-    video = create(:video, title: 'The Bunny Ears Technique', tutorial: tutorial)
-    user = create(:user)
+    click_on 'Bookmark'
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-    visit tutorial_path(tutorial)
-
-    expect do
-      click_on 'Bookmark'
-    end.to change { UserVideo.count }.by(1)
-
+    expect(UserVideo.count).to eq(1)
+    expect(UserVideo.first.video_id).to eq(@video.id)
     expect(page).to have_content('Bookmark added to your dashboard')
   end
 
   it "can't add the same bookmark more than once" do
-    tutorial = create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
-    user = create(:user)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-    visit tutorial_path(tutorial)
-
     click_on 'Bookmark'
+
     expect(page).to have_content('Bookmark added to your dashboard')
+
     click_on 'Bookmark'
+
     expect(page).to have_content('Already in your bookmarks')
   end
 end
