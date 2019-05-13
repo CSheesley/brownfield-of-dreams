15ebcbcf610144dc27154ff114b6e99c99ae9469
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-13 14:18:22 -0600

    Adds pseudo coding set up for user can add friends spec
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -13,17 +13,20 @@ describe 'as a logged in user' do
     json_repos_response = File.open('./fixtures/user_repos.json')
     stub_request(:get, 'https://api.github.com/user/repos')
       .to_return(status: 200, body: json_repos_response)
-
-    corey = create(:user, git_key: "bananas", git_id: 43529041)
-    noah = create(:user, git_key: "apples", git_id: 34421236)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(corey)
   end
 
   context 'when I visit my dashboard' do
     it 'shows a link next to followers/followed names' do
+      corey = create(:user, git_key: "bananas", git_id: 43529041)
+      noah = create(:user, git_key: "apples", git_id: 34421236)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(corey)
 
+      visit dashboard_path
+
+      within '.followers' do
+
+      end
 
     end
   end
