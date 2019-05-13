b66e78d9a9dd38382447aa5b9c341247e2063dcf
Noah Flint <noah.flint24@gmail.com>
2019-05-13 15:15:21 -0600

    Created add_friend? method for githubber class
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- app/models/githubber.rb
+++ app/models/githubber.rb
@@ -1,8 +1,17 @@
 class Githubber
-  attr_reader :name, :url
+  attr_reader :name, :url, :git_id
 
   def initialize(githubber_data)
     @name = githubber_data['login']
     @url = githubber_data['html_url']
+    @git_id = githubber_data['id']
   end
+
+  def add_friend?
+    User.find_by(git_id: self.git_id)
+
+  end
+
+
+  
 end
