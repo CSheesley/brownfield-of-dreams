d09101ac5eaebeeffa1e48a357e263ca00c9a347
Noah Flint <noah.flint24@gmail.com>
2019-05-13 20:54:10 -0600

    Added test for friends section of users#show
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -13,33 +13,31 @@ describe 'as a logged in user' do
     json_repos_response = File.open('./fixtures/user_repos.json')
     stub_request(:get, 'https://api.github.com/user/repos')
       .to_return(status: 200, body: json_repos_response)
+
+    @user_1 = create(:user, git_key: "bananas", git_id: 43529041)
+    @user_2 = create(:user, git_key: "apples", git_id: 34421236)
+    @user_3 = create(:user, git_key: "oranges", git_id: 34906415)
+
+    visit login_path
+
+    fill_in 'session[email]', with: @user_1.email
+    fill_in 'session[password]', with: @user_1.password
+
+    click_on 'Log In'
   end
 
   context 'when I visit my dashboard' do
     it 'shows a link next to followers/followed names' do
-      user_1 = create(:user, git_key: "bananas", git_id: 43529041)
-      user_2 = create(:user, git_key: "apples", git_id: 34421236)
-      user_3 = create(:user, git_key: "oranges", git_id: 34906415)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
-      
-      visit login_path
-
-      fill_in 'session[email]', with: user_1.email
-      fill_in 'session[password]', with: user_1.password
-
-      click_on 'Log In'
-
       expect(current_path).to eq(dashboard_path)
 
-      expect(user_1.friends.count).to eq(0)
-      expect(user_2.friends.count).to eq(0)
+      expect(@user_1.friends.count).to eq(0)
+      expect(@user_2.friends.count).to eq(0)
 
       within '.followed' do
         expect(page).to have_css('.single_followed', count: 5)
         expect(page).to have_link("Add Friend", count: 1)
       end
-      
+
       within '.followers' do
         expect(page).to have_css('.follower', count: 2)
         expect(page).to have_link("Add Friend", count: 1)
@@ -48,9 +46,38 @@ describe 'as a logged in user' do
 
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_link("Add Friend", count: 1)
-      expect(user_1.friends.count).to eq(1)
-      expect(user_2.friends.count).to eq(1)
-      expect(page).to have_content("#{user_2.first_name} added as a friend!")
+      expect(@user_1.friends.count).to eq(1)
+      expect(@user_2.friends.count).to eq(1)
+      expect(page).to have_content("#{@user_2.first_name} added as a friend!")
+    end
+
+    it 'shows a list of friends' do
+      expect(current_path).to eq(dashboard_path)
+
+      expect(@user_1.friends.count).to eq(0)
+      
+      within '.followed' do
+        expect(page).to have_css('.single_followed', count: 5)
+        expect(page).to have_link("Add Friend", count: 1)
+        click_link("Add Friend")
+      end
+      
+      within '.followers' do
+        expect(page).to have_css('.follower', count: 2)
+        expect(page).to have_link("Add Friend", count: 1)
+        click_link("Add Friend")
+      end
+
+      within '.friends' do
+        expect(page).to have_css('.friend', count: 2)
+        expect(page).to have_content(@user_2.first_name)
+        expect(page).to have_content(@user_2.last_name)
+        expect(page).to have_content(@user_3.first_name)
+        expect(page).to have_content(@user_3.last_name)
+      end
+
+      expect(page).to have_link("Add Friend", count: 0)
+      expect(@user_1.friends.count).to eq(2)
     end
   end
 end
