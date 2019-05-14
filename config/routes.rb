1baeb2384d456461d53cb7b8bb986bf59b54e015
Noah Flint <noah.flint24@gmail.com>
2019-05-13 20:19:31 -0600

    Adds friends#show route and controller
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -28,12 +28,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  # get '/github_connect', to: redirect("https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT_ID']}"), as: 'github_connect'
   get '/auth/github/callback', to: 'users#update'
   get '/login/oauth/authorize', to: 'users#update'
   get '/dashboard', to: 'users#show'
 
-  get '/add_friend', to: 'friends#create'
+  get '/add_friend', to: 'friendships#create'
 
   get '/about', to: 'about#show'
 
