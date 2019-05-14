00e7a6ec612dd02146b442ce428561a8cabd20b4
Noah Flint <noah.flint24@gmail.com>
2019-05-13 20:21:10 -0600

    adds friendship spec
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -17,12 +17,28 @@ describe 'as a logged in user' do
 
   context 'when I visit my dashboard' do
     it 'shows a link next to followers/followed names' do
-      corey = create(:user, git_key: "bananas", git_id: 43529041)
-      noah = create(:user, git_key: "apples", git_id: 34421236)
+      user_1 = create(:user, git_key: "bananas", git_id: 43529041)
+      user_2 = create(:user, git_key: "apples", git_id: 34421236)
+      user_3 = create(:user, git_key: "oranges", git_id: 34906415)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(corey)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+      
+      visit login_path
 
-      visit dashboard_path
+      fill_in 'session[email]', with: user_1.email
+      fill_in 'session[password]', with: user_1.password
+
+      click_on 'Log In'
+
+      expect(current_path).to eq(dashboard_path)
+
+      expect(user_1.friends.count).to eq(0)
+      expect(user_2.friends.count).to eq(0)
+
+      within '.followed' do
+        expect(page).to have_css('.single_followed', count: 5)
+        expect(page).to have_link("Add Friend", count: 1)
+      end
       
       within '.followers' do
         expect(page).to have_css('.follower', count: 2)
@@ -30,7 +46,11 @@ describe 'as a logged in user' do
         click_link("Add Friend")
       end
 
-      # expect(current_path).to eq(dashboard)
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_link("Add Friend", count: 1)
+      expect(user_1.friends.count).to eq(1)
+      expect(user_2.friends.count).to eq(1)
+      expect(page).to have_content("#{user_2.first_name} added as a friend!")
     end
   end
 end
