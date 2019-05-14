d299432935ed6badc487faf13ee9eaa0cc463445
Noah Flint <noah.flint24@gmail.com>
2019-05-14 15:41:02 -0600

    Refactored test to only show free tutorials for visitor, and all tutorials for registered users
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- spec/features/visitors/visitor_sees_homepage_spec.rb
+++ spec/features/visitors/visitor_sees_homepage_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-describe 'Visitor' do
-  describe 'on the home page' do
-    it 'can see a list of tutorials' do
+describe 'On the home page' do
+  context 'as a visitor' do
+    it 'can see a list of only free tutorials' do
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
-
+      tutorial3 = create(:tutorial, classroom: true)
+      
       visit root_path
 
       expect(page).to have_css('.tutorial', count: 2)
@@ -14,9 +15,31 @@ describe 'Visitor' do
         expect(page).to have_css('.tutorial')
         expect(page).to have_css('.tutorial-description')
         expect(page).to have_content(tutorial1.title)
-        expect(page).to have_content(tutorial1.description)
         expect(page).to have_content(tutorial2.title)
-        expect(page).to have_content(tutorial2.description)
+        expect(page).to_not have_content(tutorial3.title)
+      end
+    end
+  end
+
+  context 'as a registered user' do
+    it 'can see a list of only free tutorials' do
+      user = create(:user)
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial)
+      tutorial3 = create(:tutorial, classroom: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 3)
+
+      within('.tutorials') do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial2.title)
+        expect(page).to have_content(tutorial3.title)
       end
     end
   end
