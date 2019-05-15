22a6cfa7b9c6c7e738bc1fcb80ab276d9457446d
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-14 19:56:21 -0600

    Upates video .set_missing_positions spec - overriding postition validation for test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe Video, type: :model do
     context '.set_missing_postions' do
       it 'will assign positions to videos where position is currently nil' do
         tutorial = create(:tutorial)
-        video_1 = Video.create!(title: "Video 1", description: "A video", tutorial_id: tutorial.id, position: 1)
-        video_2 = Video.create!(title: "Video 2", description: "Other video", tutorial_id: tutorial.id, position: nil)
-        video_3 = Video.create!(title: "Video 3", description: "Another video", tutorial_id: tutorial.id, position: nil)
+        video_1 = Video.create(title: "Video 1", description: "A video", tutorial_id: tutorial.id, position: 1)
+        video_2 = Video.new(title: "Video 2", description: "Other video", tutorial_id: tutorial.id, position: nil)
+        video_3 = Video.new(title: "Video 3", description: "Another video", tutorial_id: tutorial.id, position: nil)
+
+        # Ignores validations to assume that we had videos in the DB with position: nil
+        video_2.save(validate: false)
+        video_3.save(validate: false)
 
         expect(video_2.position).to eq(nil)
         expect(video_3.position).to eq(nil)
