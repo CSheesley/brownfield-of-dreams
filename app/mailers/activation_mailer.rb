8b166e91bd9daa8385c8c35713b437390b46d767
Noah Flint <noah.flint24@gmail.com>
2019-05-16 00:15:40 -0600

    refactored line length

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -1,6 +1,8 @@
 class ActivationMailer < ApplicationMailer
   def activate(user)
     @user = user
-    mail(to: @user.email, subject: "#{@user.first_name}, activate your account.")
+    mail(to: @user.email,
+         subject: "#{@user.first_name},
+         activate your account.")
   end
 end
