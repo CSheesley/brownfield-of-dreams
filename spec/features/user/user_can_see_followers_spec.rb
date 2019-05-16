4460bead1a7bc64534f0d56ad1d5368eb4572b7f
Noah Flint <noah.flint24@gmail.com>
2019-05-15 23:27:48 -0600

    refactored line length

--- spec/features/user/user_can_see_followers_spec.rb
+++ spec/features/user/user_can_see_followers_spec.rb
@@ -16,10 +16,12 @@ describe 'as a logged in user' do
   end
 
   context 'when I visit my dashboard - in the Github section' do
-    it 'shows a followers section, with my followers handles, which link to their profiles' do
+    it 'my followers handles are links to their profiles' do
       user = create(:user, git_key: 'bananas')
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user)
+        .and_return(user)
 
       visit dashboard_path
 
