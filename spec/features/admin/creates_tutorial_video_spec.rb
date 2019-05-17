3ed87ea164db8c0541f9f8add88204bd60afb116
Noah Flint <noah.flint24@gmail.com>
2019-05-16 23:31:00 -0600

    refactored test to use edit_admin_tutorial_path

--- spec/features/admin/creates_tutorial_video_spec.rb
+++ spec/features/admin/creates_tutorial_video_spec.rb
@@ -9,30 +9,18 @@ RSpec.describe 'as a admin on the home page' do
       allow_any_instance_of(ApplicationController)
         .to receive(:current_user)
         .and_return(admin)
-      # video = create(:video)
-      # require 'pry'; binding.pry
 
-      visit '/'
+      visit tutorial_path(tutorial)
 
-      click_on tutorial.title
-      # save_and_open_page
       click_link 'Add Video'
 
-      expect(current_path).to eq(new_admin_tutorial_video_path(tutorial))
-      expect(Video.count).to eq(0)
-      
-      fill_in 'title', with: 'video_title'
-      fill_in 'description', with: 'description'
-      fill_in 'thumbnail', with: 'thumbnail'
-      fill_in 'description', with: 'description'
-      #fill in video id?
-      fill_in 'video_id', with: '1234'
-      #expect to be associated with tutorial
+      fill_in 'video[title]', with: 'Test Video'
+      fill_in 'video[description]', with: 'Description of video.'
+      fill_in 'video[video_id]', with: 'PyHxTHN-kvU'
+      click_on 'Create Video'
 
-      click_button 'Create Video'
-      expect(Video.count).to eq(1)
-
-      expect(page).to have_content("Successfully video_title added title to #{tutuorial.title}")
+      expect(page).to have_content("#{Video.last.title} added to #{tutorial.title}")
+      expect(Video.last.title).to eq('Test Video')
     end
   end
 end
