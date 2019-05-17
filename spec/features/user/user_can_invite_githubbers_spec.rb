0828637b902a53b84525fcd33b965d4b854f285f
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-17 17:04:49 -0600

    Creates spec for inviting github users via email.
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- spec/features/user/user_can_invite_githubbers_spec.rb
+++ spec/features/user/user_can_invite_githubbers_spec.rb
@@ -13,34 +13,59 @@ describe 'as a registered user on the dashboard page' do
     json_repos_response = File.open('./fixtures/user_repos.json')
     stub_request(:get, 'https://api.github.com/user/repos')
       .to_return(status: 200, body: json_repos_response)
+  end
 
-    json_user_info_response = File.open('./fixtures/user_info.json')
-    stub_request(:get, 'https://api.github.com/users')
-      .to_return(status: 200, body: json_user_info_response)
-    end
   context 'when sending an email invitation to a github user' do
     it 'is successful if that user has an email tied to github account' do
-      # As a registered user
+      # json_user_info_response = File.open('./fixtures/user_info_with_email.json')
+      # stub_request(:get, 'https://api.github.com/users')
+      #   .to_return(status: 200, body: json_user_info_response)
+
       git_token = "d115689f194f1572f02cdad273b850aa2fa6d848"
+      user = create(:user, role: 'active', git_key: git_token, git_id: 34421236)
+
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user)
+        .and_return(user)
+
+      visit dashboard_path
+
+      click_on "Send an Invite"
+
+      expect(current_path).to eq(invite_path)
+
+      fill_in 'handle', with: "test-freind"
+
+      click_on "Send Invite"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Successfully sent invite!")
+    end
+
+    it 'fails if that user does not have an email tied to github account' do
+      # json_user_info_response = File.open('./fixtures/user_info_no_email.json')
+      # stub_request(:get, 'https://api.github.com/users')
+      #   .to_return(status: 200, body: json_user_info_response)
+
+      git_token = "d115689f194f1572f02cdad273b850aa2fa6d848"
+
       user = create(:user, role: 'active', git_key: git_token, git_id: 34421236)
       allow_any_instance_of(ApplicationController)
         .to receive(:current_user)
         .and_return(user)
 
-      # When I visit /dashboard
       visit dashboard_path
-      # And I click "Send an Invite"
+
       click_on "Send an Invite"
-      # Then I should be on /invite
+
       expect(current_path).to eq(invite_path)
-      # And when I fill in "Github Handle" with <A VALID GITHUB HANDLE>
-      fill_in 'handle', with: "n-flint"
-      # And I click on "Send Invite"
+
+      fill_in 'handle', with: "not-a-real-name"
+
       click_on "Send Invite"
-      # Then I should be on /dashboard
+
       expect(current_path).to eq(dashboard_path)
-      # And I should see a message that says "Successfully sent invite!" (if the user has an email address associated with their github account)
-      expect(page).to have_content("Successfully sent invite!")
+      expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
     end
   end
 end
