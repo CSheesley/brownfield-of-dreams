ded06ffaa4705e98293e1bba3b8116d8d93ad348
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-17 17:05:50 -0600

    Creates working API call for finding github user emails - need to refactor
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -4,12 +4,31 @@ class InviteController < ApplicationController
   end
 
   def create
-    # @user = current_user
-    # handle
-    # mailer
-    # binding.pry
-    flash[:success] = "Successfully sent invite!"
+    @user = current_user
+
+    if get_email(params[:handle])
+      # send email
+      flash[:success] = "Successfully sent invite!"
+    else
+      flash[:failure] = "The Github user you selected doesn't have an email address associated with their account."
+    end
     redirect_to dashboard_path
   end
 
+  private
+
+    def get_email(handle)
+      conn = Faraday.new("https://api.github.com/users/") do |f|
+        f.headers["Authorization"] = "TOKEN #{@user.git_key}"
+        f.adapter Faraday.default_adapter
+      end
+      response = conn.get(handle)
+
+      email = JSON.parse(response.body, symbolize_name: true)["email"]
+
+    end
+
+    # def conn
+    #   # service = service.new()
+    # end
 end
