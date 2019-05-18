a2a4232adb278416fde4709038026f5bc9d46fd8
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-17 20:27:35 -0600

    Adds model validations and tests
    Co-authored-by: Noah Flint <34421236+n-flint@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name
+  validates_presence_of :last_name
+
   enum role: %i[default admin active]
   has_secure_password
-
-
 
 end
